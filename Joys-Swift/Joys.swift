bda8eefc517c20fff93e01185b89e3bdc5cbcad3
邓琼 <523338487@qq.com>
2019-05-27 14:55:33 +0800

    适配iPhone X

--- Joys-Swift/Joys.swift
+++ Joys-Swift/Joys.swift
@@ -14,21 +14,21 @@ let SCREEN_HEIGHT = UIScreen.main.bounds.size.height
 
 
 /** 最新数据 */
-let requestLatestDataURL = "http://news-at.zhihu.com/api/4/news/latest"
+let requestLatestDataURL = "https://news-at.zhihu.com/api/4/news/latest"
 /** 某天之前的数据  需要拼接date*/
 let requestBeforeDataURL = "http://news.at.zhihu.com/api/4/news/before/"
 /** 某条数据  需要拼接id*/
-let requestNewsURL = "http://news-at.zhihu.com/api/4/news/"
+let requestNewsURL = "https://news-at.zhihu.com/api/4/news/"
 /** 主题列表 */
-let requestThemes = "http://news-at.zhihu.com/api/4/themes"
+let requestThemes = "https://news-at.zhihu.com/api/3/sections"
 /** 获取某主题的数据 需要拼接主题id*/
-let requestThemeData = "http://news-at.zhihu.com/api/4/theme/"
+let requestThemeData = "https://news-at.zhihu.com/api/3/section/"
 /** 获取热门数据 */
-let requestHotData = "http://news-at.zhihu.com/api/3/news/hot"
+let requestHotData = "https://news-at.zhihu.com/api/3/news/hot"
 /** 获取评论总数和点赞总数 */
-let requestExtraData = "http://news-at.zhihu.com/api/4/story-extra/"
+let requestExtraData = "https://news-at.zhihu.com/api/4/story-extra/"
 /** 获取长评论  需要拼接id/long-comments */
-let requestLongComments = "http://news-at.zhihu.com/api/4/story/"
+let requestLongComments = "https://news-at.zhihu.com/api/4/story/"
 
 
 
